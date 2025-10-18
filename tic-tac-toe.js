210ef2cc371a050139348056a893539b78807e12
Tyreque16 <500001903@my.fiveislands.uwi.edu>
2025-10-17 20:30:20 -0400

    Exercise 2: Add X or O on click with board tracking

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,40 @@
 
-
-
 window.onload = function() {
+  var squares = document.querySelectorAll("#board div");
+  var turn = "X";
+  var boardState = ["", "", "", "", "", "", "", "", ""]; // track X/O
 
-  const squares = document.querySelectorAll("#board div");
+  // 1️⃣ Add 'square' class to each square
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].classList.add("square");
+
+    // Store the index in a property of the square
+    squares[i].squareIndex = i;
+
+    // 2️⃣ Click event to place X or O
+    squares[i].onclick = function() {
+      var index = this.squareIndex;
+
+      // Only place X or O if empty
+      if (boardState[index] == "") {
+        boardState[index] = turn;
+        this.innerHTML = turn;
+        this.className = turn;
+
+        // Switch turns
+        if (turn == "X") {
+          turn = "O";
+        } else {
+          turn = "X";
+        }
+      }
+    };
+  }
+};
+
 
   
   squares.forEach(function(square) {
     square.classList.add("square");
   });
 };
-tic-tac-toe-master/tic-tac-toe-master/tic-tactoe.js
