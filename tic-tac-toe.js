81471b1de624c83b20d8eb08ac980f42b2351bd9
Tyreque16 <500001903@my.fiveislands.uwi.edu>
2025-10-17 20:48:56 -0400

    - Change the style when you move your mouse over a square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,40 +1,32 @@
-
 window.onload = function() {
   var squares = document.querySelectorAll("#board div");
   var turn = "X";
   var boardState = ["", "", "", "", "", "", "", "", ""]; // track X/O
 
-  // 1️⃣ Add 'square' class to each square
+  // Loop through each square
   for (var i = 0; i < squares.length; i++) {
-    squares[i].classList.add("square");
+    squares[i].classList.add("square"); // Exercise 1: layout
+    squares[i].squareIndex = i;          // store index for click
 
-    // Store the index in a property of the square
-    squares[i].squareIndex = i;
-
-    // 2️⃣ Click event to place X or O
+    // Exercise 2: click event to place X or O
     squares[i].onclick = function() {
       var index = this.squareIndex;
-
-      // Only place X or O if empty
       if (boardState[index] == "") {
         boardState[index] = turn;
         this.innerHTML = turn;
-        this.className = turn;
+        this.className = turn; // applies X or O class for CSS
 
         // Switch turns
-        if (turn == "X") {
-          turn = "O";
-        } else {
-          turn = "X";
-        }
+        turn = (turn == "X") ? "O" : "X";
       }
     };
+
+    // Exercise 3: hover effect
+    squares[i].onmouseover = function() {
+      this.classList.add("hover");
+    };
+    squares[i].onmouseout = function() {
+      this.classList.remove("hover");
+    };
   }
-};
-
-
-  
-  squares.forEach(function(square) {
-    square.classList.add("square");
-  });
 };
