34bb0b6265ef5f0526b1cca9f4062f60ed263c85
Tyreque16 <500001903@my.fiveislands.uwi.edu>
2025-10-17 20:00:39 -0400

    Added tic-tac-toe.js to style squares

---
+++ tic-tac-toe.js
@@ -0,0 +1,13 @@
+
+
+
+window.onload = function() {
+
+  const squares = document.querySelectorAll("#board div");
+
+  
+  squares.forEach(function(square) {
+    square.classList.add("square");
+  });
+};
+tic-tac-toe-master/tic-tac-toe-master/tic-tactoe.js
